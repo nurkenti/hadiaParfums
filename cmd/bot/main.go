a250336ef39c2f82c2d83028842f853b5af9823f
Nurken <bek.nurken12@mail.ru>
2026-06-25 03:03:08 +0500

    add Godotenv for token

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"log"
+	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = true // Для max ditales
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
